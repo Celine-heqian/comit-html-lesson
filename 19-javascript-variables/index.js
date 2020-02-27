0c3e4165cc1ddfbdf7d4f154f203959903319c53
Celine-heqian <celine.heqian@gmail.com>
2020-02-26 17:18:09 -0700

    lesson 19 complete

---
+++ 19-javascript-variables/index.js
@@ -0,0 +1,18 @@
+// declare constants asn assign values
+let monthOfBirth = "May";
+let firstName = "Celine";
+let lastName = "He";
+
+//declare variables:
+let city, age, nextCity;
+city = "Calgary";
+age = 40;
+nextCity = city;
+
+console.log("First Name:", firstName);
+console.log("Last Name:", lastName);
+console.log("Month of Birth:", monthOfBirth);
+console.log("City:", city);
+console.log("Age:", age);
+console.log("Next city:", nextCity);
+console.log("Name:", `${firstName}` + " " + `${lastName}`);
