6e77a5c3cfe80fe1299d0b32fc3e13334af2d5f2
Celine-heqian <celine.heqian@gmail.com>
2020-02-26 17:19:14 -0700

    lesson 22 complete

---
+++ 22-javascript-conditionals/index.js
@@ -0,0 +1,12 @@
+"use strict";
+alert("Enter two numbers to see if the first is greater than the second");
+let firstNum = prompt("Please enter the first number:", 0);
+let secondNum = prompt("Please enter the second number ", 0);
+if (firstNum > secondNum) {
+  alert("The first number is greater than the second number.");
+} else if (firstNum < secondNum) {
+  alert("The first number is less than the second number.");
+} else {
+  alert("The first number is equal to the second number.");
+}
+// alert(result);
