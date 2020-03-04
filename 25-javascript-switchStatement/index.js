611bff5ee898b564a8b65d589f35582596856444
Celine-heqian <celine.heqian@gmail.com>
2020-03-04 13:59:23 -0700

    Lesson 25 complete

---
+++ 25-javascript-switchStatement/index.js
@@ -0,0 +1,40 @@
+let weekdayNumber = 3;
+if (weekdayNumber == 1) {
+  console.log("Today is Monday.");
+} else if (weekdayNumber == 2) {
+  console.log("Today is Thuesday.");
+} else if (weekdayNumber == 3) {
+  console.log("Today is Wendensday.");
+} else if (weekdayNumber == 4) {
+  console.log("Today is Thursday.");
+} else if (weekdayNumber == 5) {
+  console.log("Today is Friday.");
+} else if (weekdayNumber == 6) {
+  console.log("Today is Saturday.");
+} else if (weekdayNumber == 7) {
+  console.log("Today is Sunday.");
+}
+
+switch (weekdayNumber) {
+  case 1:
+    console.log("Today is Monday.");
+    break;
+  case 2:
+    console.log("Today is Thuesday.");
+    break;
+  case 3:
+    console.log("Today is Wendensday.");
+    break;
+  case 4:
+    console.log("Today is Thursday.");
+    break;
+  case 5:
+    console.log("Today is Friday.");
+    break;
+  case 6:
+    console.log("Today is Saturday.");
+    break;
+  case 7:
+    console.log("Today is Sunday.");
+    break;
+}
